e54237b926241ce609af38258194b54a0be41c82
Denis <den9695@list.ru>
2021-07-22 16:10:52 +0300

    hw

--- src/main/java/HW2/Main.java
+++ src/main/java/HW2/Main.java
@@ -1,5 +1,45 @@
 package HW2;
 
 public class Main {
-    //1234
+    public static int arraySum(String[][] twoDimensionalArray) throws MyArraySizeException, MyArrayDataException {
+        if (twoDimensionalArray.length != 4) {
+            throw new MyArraySizeException("Длина массива != 4");
+        }
+
+        for (String[] array : twoDimensionalArray) {
+            if (array.length != 4) {
+                throw new MyArraySizeException("Высота массива != 4");
+            }
+        }
+
+        int sum = 0;
+        for (int i = 0; i < twoDimensionalArray.length; i++) {
+            for (int j = 0; j < twoDimensionalArray[i].length; j++) {
+                try {
+                    sum += Integer.parseInt(twoDimensionalArray[i][j]);
+                } catch (NumberFormatException e) {
+                    throw new MyArrayDataException("Не число в элементе:" + i + "," + j);
+                }
+            }
+        }
+        return sum;
+    }
+
+    public static void main(String[] args) {
+
+        String[][] SArray =
+                        {{"1", "3", "5", "6"},
+                        {"1", "3", "5", "6"},
+                        {"1", "3", "5", "6"},
+                        {"1", "3", "5", "6"}};
+
+        try {
+            System.out.println(arraySum(SArray));
+        } catch (MyArraySizeException e) {
+            e.printStackTrace();
+        } catch (MyArrayDataException e) {
+            e.printStackTrace();
+        }
+
+    }
 }
